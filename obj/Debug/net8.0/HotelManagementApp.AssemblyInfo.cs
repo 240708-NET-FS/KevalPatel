b228045a5d9e27bed3443f5e43052a74c7de8efb
Keval Patel <kevalpatel@Kevals-MacBook-Pro.local>
2024-07-26 14:30:19 -0400

    gitignore moved again

--- obj/Debug/net8.0/HotelManagementApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/HotelManagementApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelManagementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd638f9e6e106f78b0d08966eebdde9c7d6bb1ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9385c79847f55dbdb635d90b257224d5a956aafd")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelManagementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelManagementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
